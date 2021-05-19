475356cd5c52cb6bd83fa5000634306d4958f3b7
Bama Qyandija Deandra <bamaqyan@gmail.com>
2021-05-19 15:10:03 +0700

    method firebase

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -108,7 +108,49 @@
         });
         @endif
     </script>
+
+ <!-- ini biar kita bisa masuk firebase jelita 
  
+    <script>
+      // Initialize Firebase
+      var firebaseConfig = {
+        apiKey: "AIzaSyAJqgQ9d46iuQnkSkN0UBSlC30akvaUf9g",
+        authDomain: "jelita-f8a34.firebaseapp.com",
+        databaseURL: "https://jelita-f8a34-default-rtdb.firebaseio.com",
+        projectId: "jelita-f8a34",
+        storageBucket: "jelita-f8a34.appspot.com",
+        messagingSenderId: "280826785622",
+        appId: "1:280826785622:web:659986e83464baea1af0f7",
+        measurementId: "G-WSJ8TH0RZ1"}; 
+        firebase.initializeApp(config);
+      var facebookProvider = new firebase.auth.FacebookAuthProvider();
+      var googleProvider = new firebase.auth.GoogleAuthProvider();
+      var facebookCallbackLink = '/login/facebook/callback';
+      var googleCallbackLink = '/login/google/callback';
+      async function socialSignin(provider) {
+        var socialProvider = null;
+        if (provider == "facebook") {
+          socialProvider = facebookProvider;
+          document.getElementById('social-login-form').action = facebookCallbackLink;
+        } else if (provider == "google") {
+          socialProvider = googleProvider;
+          document.getElementById('social-login-form').action = googleCallbackLink;
+        } else {
+          return;
+        }
+        firebase.auth().signInWithPopup(socialProvider).then(function(result) {
+          result.user.getIdToken().then(function(result) {
+            document.getElementById('social-login-tokenId').value = result;
+            document.getElementById('social-login-form').submit();
+          });
+        }).catch(function(error) {
+          // do error handling
+          console.log(error);
+        });
+      }
+      </script>
+      -->
+
 </body>
 
 </html>
